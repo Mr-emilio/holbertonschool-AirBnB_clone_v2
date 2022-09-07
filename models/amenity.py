4e56472fee3fe087b52cbab905b9d3346d40404d
Mr-emilio <pablopeag@hotmail.com>
2022-09-07 10:10:28 -0500

    amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from os import getenv
 
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 class Amenity(BaseModel):
-    name = ""
+    """class amenity"""
+    __tablename__ = 'amenities'
+    if HBNB_TYPE_STORAGE == "db":
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
