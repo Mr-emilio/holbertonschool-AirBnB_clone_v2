e37ca62154fdeaa35a272440c23e6790d4721740
Mr-emilio <pablopeag@hotmail.com>
2022-09-07 15:01:12 -0500

    city file

--- models/city.py
+++ models/city.py
@@ -6,9 +6,21 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if HBNB_TYPE_STORAGE == "db":
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        name = ""
+        state_id = ""
+
+    if HBNB_TYPE_STORAGE == "db":
+        def __init__(self, *args, **kwargs):
+            """initializes city"""
+            super().__init__(*args, **kwargs)
