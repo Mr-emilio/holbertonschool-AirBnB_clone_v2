87767fa2afa45fa520ca122aa8f4e073b04ac8a3
Viktor <viktorkrilldj@gmail.com>
2022-09-07 12:25:02 -0500

    if cls

--- models/state.py
+++ models/state.py
@@ -4,14 +4,19 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    else:
+        name = ""
 
     @property
     def cities(self):
