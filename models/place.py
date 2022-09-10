b9c769d033494559a6b988feca18fd01750a1b81
Mr-emilio <pablopeag@hotmail.com>
2022-09-10 03:25:39 -0500

    add relationship

--- models/place.py
+++ models/place.py
@@ -35,10 +35,12 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
+        reviews = relationship("Review", cascade="all,delete",
+                backref=backref("place", cascade="all,delete"),
+                passive_deletes=True)
         amenities = relationship("Amenity", secondary="place_amenity",
-                                 backref="place_amenities",
-                                 viewonly=False)
+                back_populates="place_amenities",
+                viewonly=False)
     else:
         city_id = ""
         user_id = ""
