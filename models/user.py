301249d9fcdd4866ae0c942805167564bc821c54
Mr-emilio <pablopeag@hotmail.com>
2022-09-10 14:17:08 -0500

    new relationship

--- models/user.py
+++ models/user.py
@@ -16,14 +16,16 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")        
+        places = relationship("Place", cascade="all,delete",
+                backref=backref("user", cascade="all,delete"),
+                passive_deletes=True,
+                single_parent=True)
+        reviews = relationship("Review", cascade="all,delete",
+                backref=backref("user", cascade="all,delete"),
+                passive_deletes=True,
+                single_parent=True)
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
