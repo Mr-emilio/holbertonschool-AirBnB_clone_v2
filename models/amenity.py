9a7416692fc0062e959d3475bef36863e1664e56
Mr-emilio <pablopeag@hotmail.com>
2022-09-12 11:52:44 -0500

    place amenities relationship

--- models/amenity.py
+++ models/amenity.py
@@ -6,11 +6,16 @@ from os import getenv
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
     """class amenity"""
     __tablename__ = 'amenities'
     if HBNB_TYPE_STORAGE == "db":
         name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="amenities")
     else:
         name = ""
 
